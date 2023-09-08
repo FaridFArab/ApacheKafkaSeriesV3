227acb5b2d3e97bde6cfdc690b420cd7bc84d2da
Home <faridarab4218@gmail.com>
2023-09-08 19:31:47 +0330

    (Feature) Implement consumer to add event source from topic to OpenSearch.

--- kafka-consumer-opensearch/src/main/java/io/conduktor/demos/kafka/opensearch/OpenSearchConsumer.java
+++ kafka-consumer-opensearch/src/main/java/io/conduktor/demos/kafka/opensearch/OpenSearchConsumer.java
@@ -6,16 +6,27 @@ import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.CredentialsProvider;
 import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.client.DefaultConnectionKeepAliveStrategy;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.opensearch.action.index.IndexRequest;
+import org.opensearch.action.index.IndexResponse;
 import org.opensearch.client.RequestOptions;
 import org.opensearch.client.RestClient;
 import org.opensearch.client.RestHighLevelClient;
 import org.opensearch.client.indices.CreateIndexRequest;
 import org.opensearch.client.indices.GetIndexRequest;
+import org.opensearch.common.xcontent.XContentType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.net.URI;
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
 
 public class OpenSearchConsumer {
 
@@ -53,8 +64,11 @@ public class OpenSearchConsumer {
         // create an OpenSearch Client
         RestHighLevelClient openSearchClient = createOpenSearchClient();
 
+        // create consumer configs
+        KafkaConsumer<String, String > kafkaConsumer = createKafkaConsumer();
+
         // we need to create the index on OpenSearch if it doesn't exist already
-        try (openSearchClient)
+        try (openSearchClient; kafkaConsumer)
         {
             if(!openSearchClient.indices().exists(new GetIndexRequest("wikimedia"), RequestOptions.DEFAULT))
             {
@@ -66,7 +80,37 @@ public class OpenSearchConsumer {
                 logger.info("The Wikimedia Index already exists!");
             }
 
+            // subscribe consumer to specific topic
+            kafkaConsumer.subscribe(Collections.singleton("wikimedia.recentchange"));
+
+            while (true){
+                ConsumerRecords<String, String> records = kafkaConsumer.poll(Duration.ofMillis(3000));
+                logger.info("Received " + records.count() + " record(s)");
+
+                for(ConsumerRecord<String, String> record: records){
+                    // send record into OpenSearch
+                    IndexRequest indexRequest = new IndexRequest("wikimedia").source(record.value(), XContentType.JSON);
+                    IndexResponse indexResponse =  openSearchClient.index(indexRequest, RequestOptions.DEFAULT);
+
+                    logger.info(indexResponse.getId());
+                }
+            }
+
 //        openSearchClient.close();
         }
+    }
+
+    private static KafkaConsumer<String, String> createKafkaConsumer()
+    {
+        logger.info("I'm a Kafka Consumer");
+
+        Properties properties = new Properties();
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,"localhost:9092");
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG,"consumer-open-search");
+        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG,"latest");
+
+        return new KafkaConsumer<>(properties);
     }
 }
